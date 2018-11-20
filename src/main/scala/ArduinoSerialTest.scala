91f5af18ef533da120031545d472c27064584866
Ilya Kazovsky <gkazovsky@gmail.com>
2018-11-20 22:59:35 +0200

    Stream from adruino is working. Nothing is sent to arduino

--- src/main/scala/ArduinoSerialTest.scala
+++ src/main/scala/ArduinoSerialTest.scala
@@ -1,3 +1,6 @@
+
+import java.io.{File, FilenameFilter}
+
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.event.Logging
@@ -5,57 +8,89 @@ import akka.stream.actor.ZeroRequestStrategy
 import akka.stream.scaladsl._
 import akka.stream.{ActorMaterializer, Attributes, ClosedShape}
 import akka.util.ByteString
-import com.github.jarlakxen.reactive.serial.ReactiveSerial
-import org.reactivestreams.Publisher
+import com.fazecast.jSerialComm.SerialPort
+import com.github.jarlakxen.reactive.serial.{Port, ReactiveSerial}
+import org.reactivestreams.{Publisher, Subscriber}
 
-import java.io.File
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 object ArduinoSerialTest extends App {
+  val encode: String => ByteString = ByteString(_, "utf-8")
+  val separator = "\n"
+  val sourceDelimiter = encode(";")
 
-  implicit val actorSystem = ActorSystem("ReactiveSerial")
+  implicit val actorSystem = ActorSystem("RaspberryFarm")
   implicit val materializer = ActorMaterializer()
 
-  val ports = new File("/dev/")
-    .listFiles
+  val isArduinoLog: String => Boolean = _.trim.startsWith("log:")
+
+  val baudRate = 9600
+
+  val ports = new File("/dev")
+    .listFiles(new FilenameFilter {
+      override def accept(file: File, s: String): Boolean = s.startsWith("ttyACM")
+    })
     .toList
-    .filter(_.getName.startsWith("ttyACM"))
-    .map(_.getAbsolutePath)
-    .map(ReactiveSerial.port)
-    .map(ReactiveSerial(_, 115200))
-    .map(s => (s, s.publisher(bufferSize = 100)))
-    .collect{ case (s: ReactiveSerial, p: Publisher[_]) => (p, s.subscriber(ZeroRequestStrategy)) }
-    .map(x => (Source.fromPublisher(x._1), Sink.fromSubscriber(x._2)))
+    .map(f => SerialPort.getCommPort(f.getAbsolutePath))
+    .map(new Port(_))
+    .map(ReactiveSerial(_, baudRate))
+    .map(s => (s.publisher(bufferSize = 100), s.subscriber(ZeroRequestStrategy)))
     .collect {
-      case (src: Source[ByteString, _], sink: Sink[ByteString, _]) =>
-        RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      case (p: Publisher[ByteString], s: Subscriber[ByteString]) =>
+        val source = Source.fromPublisher(p)
+        val sink = Sink.fromSubscriber(s)
+
+        RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
           import GraphDSL.Implicits._
 
-          val print = Flow[String].log("arduino-data")
-          .withAttributes(
-            Attributes.logLevels(
-              onElement = Logging.WarningLevel,
-              onFinish = Logging.InfoLevel,
-              onFailure = Logging.DebugLevel
-            )
-          )
+          val bcast = builder.add(Broadcast[String](2))
 
-          val decode = Flow[ByteString].via(
-            Framing.delimiter(ByteString("\r\n"), maximumFrameLength = 100, allowTruncation = true)
-          ).map(_.utf8String)
+          source ~> decode ~> bcast
 
-          val reverse = Flow[String].map(_.reverse)
-          val encode = Flow.fromFunction[String, ByteString](s => ByteString(s, "utf-8"))
-
-          val concat = builder.add(Concat[String]())
-
-          val start = Source.single("Start")
-
-                             start ~> print ~> concat
-          src ~> decode ~> print ~> reverse ~> concat ~> encode ~> sink
+          bcast ~> process ~> logArduinoData ~> finishMessage ~> sink
+          bcast ~> logArduinoDebug ~> Sink.ignore
 
           ClosedShape
         })
     }
     .map(_.run)
 
+  def finishMessage(implicit builder: GraphDSL.Builder[NotUsed]) =
+    Flow[String]
+      .filter(!_.isEmpty)
+      .map(_ + separator)
+      .map(encode)
+
+
+  def process(implicit builder: GraphDSL.Builder[NotUsed]) =
+    Flow[String].filter(!isArduinoLog(_)).map(_.toUpperCase)
+
+  def decode(implicit builder: GraphDSL.Builder[NotUsed]) =
+    Flow[ByteString].via(
+      Framing.delimiter(sourceDelimiter, maximumFrameLength = 200, allowTruncation = true)
+    ).map(_.utf8String)
+      .throttle(1, 1 second)
+
+  def logArduinoData(implicit builder: GraphDSL.Builder[NotUsed]) =
+    Flow[String]
+      .log("arduino-data")
+      .withAttributes(
+        Attributes.logLevels(
+          onElement = Logging.WarningLevel,
+          onFinish = Logging.InfoLevel,
+          onFailure = Logging.DebugLevel
+        )
+      )
+
+  def logArduinoDebug(implicit builder: GraphDSL.Builder[NotUsed]) =
+    Flow[String].filter(isArduinoLog)
+      .log("arduino-log")
+      .withAttributes(
+        Attributes.logLevels(
+          onElement = Logging.InfoLevel,
+          onFinish = Logging.InfoLevel,
+          onFailure = Logging.DebugLevel
+        )
+      )
 }
