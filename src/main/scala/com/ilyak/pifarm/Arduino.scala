d1f7b904f09b39fe3ea30c568b27aec48f3281e4
Ilya Kazovsky <gkazovsky@gmail.com>
2018-12-16 20:32:36 +0200

    Flow refactoring. More logs

--- src/main/scala/com/ilyak/pifarm/Arduino.scala
+++ src/main/scala/com/ilyak/pifarm/Arduino.scala
@@ -2,33 +2,26 @@ package com.ilyak.pifarm
 
 import java.nio.charset.StandardCharsets
 
+import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, Framing, RestartFlow}
+import akka.stream.FlowShape
+import akka.stream.scaladsl.{Flow, Framing, GraphDSL, RestartFlow, Sink}
 import akka.util.ByteString
 import com.fazecast.jSerialComm.SerialPort
 import com.ilyak.pifarm.data.ArduinoEvent
-import com.ilyak.pifarm.shapes.{ArduinoConnector, RateMonitor, SuckEventFlow}
+import com.ilyak.pifarm.shapes.{ArduinoConnector, EventSuction, RateGuard}
 
+import scala.concurrent.duration.FiniteDuration
 import scala.language.postfixOps
 
-class Arduino private(port: Port, baudRate: Int = 9600)(implicit actorSystem: ActorSystem) {
-
+class Arduino private(port: Port, baudRate: Int = 9600)
+                     (implicit actorSystem: ActorSystem) {
+  import Arduino.FlowBits._
   import scala.concurrent.duration._
-
-  type Event = ArduinoEvent
 
   val interval = 1200 milliseconds
 
   val name = port.name
-
-  val charset = StandardCharsets.ISO_8859_1
-  val encode: String => ByteString = ByteString(_, charset)
-  val terminatorSymbol = ";"
-  val terminator = encode(terminatorSymbol)
-  val resetCmd = encode("cmd: reset" + terminatorSymbol)
-
-  val isEvent: String => Boolean = _.contains(" value: ")
-  val toMessage: Event => String = f => s"value: $f"
 
   def restartFlow[In, Out](minBackoff: FiniteDuration, maxBackoff: FiniteDuration): (() ⇒ Flow[In, Out, _]) => Flow[In, Out, _] =
     RestartFlow.withBackoff[In, Out](
@@ -38,35 +31,61 @@ class Arduino private(port: Port, baudRate: Int = 9600)(implicit actorSystem: Ac
     )
 
   val flow = restartFlow(500 milliseconds, 2 seconds) { () =>
-    Flow[String]
-      .map(_ + terminatorSymbol)
-      .map(encode)
-      .mapConcat[ByteString](b => b.grouped(16).toList)
-      .via(ArduinoConnector(port, baudRate, resetCmd))
-      .via(
-        Framing.delimiter(terminator, maximumFrameLength = 200, allowTruncation = true)
-      )
-      .map(_.decodeString(charset).trim)
-      .via(SuckEventFlow(
-        interval,
-        isEvent,
-        ArduinoEvent.generate,
-        toMessage,
-        ArduinoEvent.empty
-      ))
-      .via(
-        RateMonitor[String](30 seconds, 1 minute)
-      )
-      .log(s"arduino($name)-event")
-      .withAttributes(logAttributes)
+    Flow.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+      val input = builder.add(stringToBytesFlow)
+      val arduino = ArduinoConnector(port, baudRate, resetCmd)
+      val suction = eventSuction(interval)
+      val guard = builder.add(RateGuard[String](10, 1 minute))
+      val log = Flow[String]
+        .log(s"arduino($name)-event")
+        .withAttributes(logAttributes)
+        .to(Sink.ignore)
+
+      input ~> arduino ~> frameCutter ~> decodeFlow ~> suction ~> guard.in
+      guard.out1 ~> log
+
+      FlowShape(input.in, guard.out0)
+    })
   }
 }
 
 object Arduino {
 
+  type Event = ArduinoEvent
+
   private def getPort(port: String) = new Port(SerialPort.getCommPort(port))
 
   def apply(port: String)(implicit actorSystem: ActorSystem): Arduino = new Arduino(getPort(port))
-
   def apply(port: String, baudRate: Int)(implicit actorSystem: ActorSystem): Arduino = new Arduino(getPort(port), baudRate)
+
+  private object FlowBits {
+    val charset = StandardCharsets.ISO_8859_1
+    val encode: String => ByteString = ByteString(_, charset)
+    val decodeFlow: Flow[ByteString, String, NotUsed] =
+      Flow[ByteString].map(_.decodeString(charset).trim)
+
+    val terminatorSymbol = ";"
+    val terminator = encode(terminatorSymbol)
+    val frameCutter = Framing.delimiter(terminator, maximumFrameLength = 200, allowTruncation = true)
+
+    val resetCmd = encode("cmd: reset" + terminatorSymbol)
+
+    val isEvent: String => Boolean = _.contains(" value: ")
+    val toMessage: Event => String = f => s"value: $f"
+
+    val stringToBytesFlow = Flow[String]
+      .map(_ + terminatorSymbol)
+      .map(encode)
+      .mapConcat[ByteString](b => b.grouped(16).toList)
+
+    def eventSuction(interval: FiniteDuration) =
+      EventSuction(
+        interval,
+        isEvent,
+        ArduinoEvent.generate,
+        toMessage,
+        ArduinoEvent.empty
+      )
+  }
 }
