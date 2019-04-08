c0c139d391ef3b300731ee86145c2bdb81d33b5c
Ilya Kazovsky <gkazovsky@gmail.com>
2019-04-08 23:27:32 +0300

    GraphSpec updated with Configuration.Graph creation methods

--- src/test/scala/test/builder/GraphSpecs.scala
+++ src/test/scala/test/builder/GraphSpecs.scala
@@ -1,6 +1,7 @@
 package test.builder
 
 import akka.stream.scaladsl.RunnableGraph
+import com.ilyak.pifarm.flow.configuration.{ BlockType, Configuration }
 import com.ilyak.pifarm.flow.configuration.Configuration.Graph
 import org.scalatest.Matchers
 import org.scalatest.enablers.Emptiness
@@ -16,4 +17,32 @@ trait GraphSpecs {
       builder.inSlots == 0 &&
       builder.unwiredOuts == 0
   }
+
+  def simpleGraph: Graph = Graph(
+    Seq(simpleFlow()),
+    List("in"),
+    List("out"),
+    Map.empty
+  )
+
+  def simpleFlow(n: Int = 1): Configuration.Node = Configuration.Node(
+    n.toString,
+    List("in"),
+    List("out"),
+    Configuration.MetaData(
+      Some(s"test-flow-$n"),
+      None,
+      BlockType.Automaton,
+      "test",
+      "flow",
+      ""
+    )
+  )
+
+  def multiGraph(n: Int = 5): Graph = Graph(
+    (1 to n).map(simpleFlow),
+    List("in"),
+    List("out"),
+    Map.empty
+  )
 }
