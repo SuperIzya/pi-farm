3ac7f690f904b00be10d1e7e0bc00c842ea8fb76
Ilya Kazovsky <gkazovsky@gmail.com>
2019-05-11 00:49:54 +0300

    Intermediate commit. Arduino sketch is uploaded successfully.
    Need to load configuration before real data will flow.

--- common/src/main/scala/com/ilyak/pifarm/driver/LoaderActor.scala
+++ common/src/main/scala/com/ilyak/pifarm/driver/LoaderActor.scala
@@ -4,6 +4,7 @@ import akka.actor.SupervisorStrategy.Restart
 import akka.actor.{ Actor, ActorLogging, ActorRef, OneForOneStrategy, PoisonPill, Props }
 import com.ilyak.pifarm.driver.LoaderActor.{ CommandExecutor, Exec, ExecRes }
 
+import scala.concurrent.TimeoutException
 import scala.concurrent.duration.Duration
 
 class LoaderActor extends Actor with ActorLogging {
@@ -15,6 +16,10 @@ class LoaderActor extends Actor with ActorLogging {
     case cmd: String =>
       waiter = sender()
       executor ! Exec(cmd, 0)
+    case ExecRes(_, true, count) =>
+      log.debug(s"Attempt $count succeed")
+      waiter ! true
+      self ! PoisonPill
     case ExecRes(cmd, res, count) if !res && count < LoaderActor.maxAttempts =>
       log.debug(s"Attempt $count failed. Trying again")
       executor ! Exec(cmd, count + 1)
@@ -49,11 +54,13 @@ object LoaderActor {
         sender() ! ExecRes(cmd, false, LoaderActor.maxAttempts)
       case Exec(cmd, count) =>
         log.info(s"======== Executing '$cmd'")
+
         process = cmd run ProcessLogger(log.info, log.error)
 
         val res = process.exitValue()
         process = null
         sender() ! ExecRes(cmd, res == 0, count)
+        log.info(s"======== Executed with result $res ($cmd)")
     }
 
     override def postStop(): Unit = {
@@ -61,8 +68,10 @@ object LoaderActor {
       if(process != null) process.destroy()
     }
 
-    override def preRestart(reason: Throwable, message: Option[Any]): Unit =
-      message.foreach(self.forward(_))
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit = reason match {
+      case _: TimeoutException =>
+      case _ => message.foreach(self.forward(_))
+    }
   }
 
 }
