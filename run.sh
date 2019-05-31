158704f23dfda9bf9482c8b979c42ca8427f7e9d
Ilya Kazovsky <gkazovsky@gmail.com>
2019-05-31 06:26:47 +0300

    Before release to raspberry-pi

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 cd $(dirname $0)
 git pull || true
-npm i & npm start || true
+npm i && npm start || true
 cd common
-npm start || true
+npm i && npm start || true
 cd ../plugins/servo/
-npm start || true
+npm i && npm start || true
 cd ../temperature
-npm start || true
+npm i && npm start || true
 cd $(dirname $0)
 JAVA_OPTS="-Xmx512m -Xms512m" sbt runAll d
 
