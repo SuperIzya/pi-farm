a006f60d9cf08ed2dadaa6b846e7073fc420cdb9
Ilya Kazovsky <gkazovsky@gmail.com>
2019-06-03 02:16:11 +0300

    Blocking write to USB

--- common/src/main/scala/com/ilyak/pifarm/Port.scala
+++ common/src/main/scala/com/ilyak/pifarm/Port.scala
@@ -1,15 +1,20 @@
 package com.ilyak.pifarm
 
 import java.io.IOException
+import java.util.concurrent.Semaphore
 
 import akka.util.ByteString
 import com.fazecast.jSerialComm.{ SerialPort, SerialPortDataListener, SerialPortEvent }
 
+import scala.concurrent.ExecutionContext
 import scala.util.{ Failure, Success, Try }
 
 class Port(serialPort: SerialPort) {
 
   val name: String = serialPort.getSystemPortName
+
+  val semaphore = new Semaphore(1)
+
 
   def open(rate: Int): Try[Boolean] = {
     if(!serialPort.isOpen) {
@@ -24,10 +29,12 @@ class Port(serialPort: SerialPort) {
     if (!serialPort.closePort()) Failure(new IOException(s"Failed to close port $name"))
     else Success(true)
   }
-  def write(bytes: ByteString): Try[Int] = {
+  def write(bytes: ByteString)(implicit ex: ExecutionContext): Try[Int] = {
     serialPort.writeBytes(bytes.toArray, bytes.size) match {
       case -1 => Failure(new IOException(s"Failed to write to port $name"))
       case written =>
+        semaphore.acquire()
+        semaphore.release()
         Success(written)
     }
   }
@@ -43,17 +50,24 @@ class Port(serialPort: SerialPort) {
 
   def onDataAvailable(action: ByteString => Unit, fail: Failure[_] => Unit): Boolean = {
     serialPort.addDataListener(new SerialPortDataListener{
-      override def getListeningEvents: Int = SerialPort.LISTENING_EVENT_DATA_AVAILABLE
+      override def getListeningEvents: Int =
+        SerialPort.LISTENING_EVENT_DATA_AVAILABLE | SerialPort.LISTENING_EVENT_DATA_WRITTEN
 
       override def serialEvent(event: SerialPortEvent): Unit = {
-        while(serialPort.bytesAvailable() > 0) {
-          val buffer = new Array[Byte](16)
-          read(buffer) match {
-            case Success(_) =>
-              action(ByteString(buffer).takeWhile(_ > 0))
-            case f: Failure[_] =>
-              fail(f)
-          }
+        event.getEventType match {
+          case SerialPort.LISTENING_EVENT_DATA_AVAILABLE =>
+            while (serialPort.bytesAvailable() > 0) {
+              val buffer = new Array[Byte](16)
+              read(buffer) match {
+                case Success(_) =>
+                  action(ByteString(buffer).takeWhile(_ > 0))
+                case f: Failure[_] =>
+                  fail(f)
+              }
+            }
+          case SerialPort.LISTENING_EVENT_DATA_WRITTEN =>
+            // TODO: Add synchronization ping
+            if(!semaphore.tryAcquire()) semaphore.release()
         }
       }
     })
