6dd7da298f41dbea0855741f13a5c037894a2867
Ilya Kazovsky <gkazovsky@gmail.com>
2019-07-11 01:33:50 +0300

    Custom nodes registered in GEditor

--- web/components/configurations/register-nodes.jsx
+++ web/components/configurations/register-nodes.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import {RegisterNode} from 'gg-editor';
+import { RegisterNode } from 'gg-editor';
 import { connectNode, connectNodesList } from './store';
 
-const RegisterNodeComponent = ({node: {name}}) => (
-  <RegisterNode name={name}
-                extend={'flow-capsule'}
-                config={{}}
-                />
-);
+const RegisterNodeComponent = ({ node: { name, inputs, outputs } }) => {
+  const ins = inputs || [];
+  const outs = outputs || [];
+  const inLen = ins.length;
+  const outLen = outs.length;
+  return (
+    <RegisterNode name={name}
+                  extend={'flow-rect'}
+                  config={{
+                    anchor: [
+                      ...ins.map((x, i) => [(i + 1) / (inLen + 1), 0]),
+                      ...outs.map((x, i) => [(i + 1) / (outLen + 1), 1])
+                    ]
+                  }}
+    />
+  );
+};
 
 const RegNode = connectNode(RegisterNodeComponent);
 
-const RegisterNodes = ({count}) => (
+const RegisterNodes = ({ count }) => (
   <React.Fragment>
     {new Array(count).fill(0).map((x, i) => <RegNode index={i} key={i}/>)}
   </React.Fragment>
