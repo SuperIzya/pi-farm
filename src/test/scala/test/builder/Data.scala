b497c3cf14631cf977045a9b555a778060b4065c
Ilya Kazovsky <gkazovsky@gmail.com>
2019-07-23 18:06:37 +0300

    More correct work with connection units

--- src/test/scala/test/builder/Data.scala
+++ src/test/scala/test/builder/Data.scala
@@ -1,9 +1,18 @@
 package test.builder
 
-import com.ilyak.pifarm.{Command, Measurement}
+import com.ilyak.pifarm.{ Command, Measurement, Units }
 
 object Data {
   case class TestData(value: Float) extends Measurement[Float]
-  case object Test1 extends Command ("test1")
+  object TestData {
+    implicit val units = new Units[TestData] {
+      override val name: String = "TestData"
+    }
+  }
+  case object Test1 extends Command ("test1") {
+    implicit val unit = new Units[Test1.type] {
+      override val name: String = "Test1"
+    }
+  }
 
 }
