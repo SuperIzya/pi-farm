92b00674de06d5e010cff93ae7eb1db47ad9bfd0
ilya <ikazovsky@liveintent.com>
2025-08-20 19:35:26 +0200

    take2: linting

--- modules/ui/src/client/data.ts
+++ modules/ui/src/client/data.ts
@@ -1,32 +1,38 @@
-import type {Controller, ControllerType, PeripheryType} from '../types'
-import type {TransportObj} from "./types";
+import type { Controller, ControllerType, PeripheryType } from '../types'
+import type { TransportObj } from './types'
 
-export const dataNames = ['peripheryType', 'controllerType', 'peripheryTypes', 'controllerTypes', 'controller', 'controllers'] as const
+export const dataNames = [
+  'peripheryType',
+  'controllerType',
+  'peripheryTypes',
+  'controllerTypes',
+  'controller',
+  'controllers'
+] as const
 
 export type DataNames = (typeof dataNames)[number]
 
 export type TypedData<K extends DataNames, T> = TransportObj<K, T>
 
 export type Data =
-    | TypedData<'peripheryType', PeripheryType>
-    | TypedData<'controllerType', ControllerType>
-    | TypedData<'controllerTypes', ControllerType[]>
-    | TypedData<'peripheryTypes', PeripheryType[]>
-    | TypedData<'controller', Controller>
-    | TypedData<'controllers', Controller[]>
+  | TypedData<'peripheryType', PeripheryType>
+  | TypedData<'controllerType', ControllerType>
+  | TypedData<'controllerTypes', ControllerType[]>
+  | TypedData<'peripheryTypes', PeripheryType[]>
+  | TypedData<'controller', Controller>
+  | TypedData<'controllers', Controller[]>
 
 type AllData<D extends Data> = D extends Data
-    ? D extends TypedData<infer A, infer B>
-        ? B
-        : never
+  ? D extends TypedData<infer _A, infer B>
+    ? B
     : never
+  : never
 type AllDataTypes = AllData<Data>
 
-type FindDataType<T extends DataNames, D extends AllDataTypes> =
-    D extends AllDataTypes
-        ? TypedData<T, D> extends Data
-            ? D
-            : never
-        : never
+type FindDataType<T extends DataNames, D extends AllDataTypes> = D extends AllDataTypes
+  ? TypedData<T, D> extends Data
+    ? D
+    : never
+  : never
 
 export type ExtractData<T extends DataNames> = FindDataType<T, AllDataTypes>
