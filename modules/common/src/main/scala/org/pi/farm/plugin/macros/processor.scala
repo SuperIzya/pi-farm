7e757de74c4293b3f603cbbcf14280626f59cebb
ilya <ikazovsky@liveintent.com>
2026-04-08 19:45:59 +0200

    scala 3: processor annotation macro. WIP

--- modules/common/src/main/scala/org/pi/farm/plugin/macros/processor.scala
+++ modules/common/src/main/scala/org/pi/farm/plugin/macros/processor.scala
@@ -1,5 +1,6 @@
 package org.pi.farm.plugin.macros
 
+import org.pi.farm.plugin.{Inlet, Outlet}
 import scala.quoted.*
 import scala.annotation.MacroAnnotation
 
@@ -15,13 +16,44 @@ final class processor(name: String, description: String) extends MacroAnnotation
     companion: Option[quotes.reflect.Definition]
   ): List[quotes.reflect.Definition] = {
     import quotes.reflect.*
-    /* println(s"""
-         |================================
-         |${definition}
-         |
-         |${companion.map(_.show)}
-         |================================
-         |""".stripMargin) */
+    println(s"""
+             |================================
+             |${definition}
+             |
+             |${companion.map(_.show)}
+             |================================
+             |""".stripMargin)
+
+    val newDef = definition match {
+      case d @ ClassDef(moduleName, _, _, name, dd) =>
+
+        val inlets = dd.collect {
+          case v @ ValDef(name, tpe, rhs) =>
+            tpe.tpe.asType match {
+              case '[Inlet[t]]  => Some(name -> "inlet")
+              case '[Outlet[t]] => Some(name -> "outlet")
+              case _            => None
+            }
+        }.flatten
+
+        println(s"""
+                 |================================
+                 |name = $name
+                 |moduleName = $moduleName                  
+                 |inlets = ${inlets.mkString("\n")}
+                 |dd = ${dd.mkString("\n\t")}
+                 |
+                 |================================
+                 |""".stripMargin)
+
+        d
+      case _ =>
+        report.errorAndAbort(
+          "The @processor annotation can only be applied to objects.",
+          definition.pos
+        )
+    }
+
     List(
       definition
     )
